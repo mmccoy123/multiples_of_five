a78aa24e6bfb8bfa8810604e66347a68736340af
Matthew McCoy <matt.d.mccoy@gmail.com>
2016-04-04 11:33:27 -0700

    Multiples of 5 Commit

---
+++ multiples_of_five.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Multiples Of 5</title>
+</head>
+<body>
+	<?php
+		for( $i = 5; $i <= 1000000; $i += 5 ) {
+			echo $i . "<br>";
+		}
+	?>
+</body>
+</html>
